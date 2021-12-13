76e7ed9d92f95ecc0259d2147dd7a2f3ba6082db
Damien Bussy <damien.bussy@etu.unilim.fr>
2021-12-13 15:31:06 +0000

    frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './general/header/header.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { UsersUpdateComponent } from './components/users-update/users-update.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsersListComponent,
+    UsersUpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
